a6eefe63b2fce4052683b3f80796ca8f0e828b8b
Arbër Hajrizaj <arber.hajrizaj@hotmail.com>
2022-09-28 21:37:42 +0200

    API Fetch done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Post from './components/Post';
 
 function App() {
   return (
     <div className="App">
-     Index
+     <Post/>
     </div>
   );
 }
